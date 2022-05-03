397469c564e14324d94cf655550e16a04780dd6e
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-05-03 08:18:41 -0300

    Usuário acessa a página inicial e não visualiza galpões

--- spec/system/user_view_home_spec.rb
+++ spec/system/user_view_home_spec.rb
@@ -13,7 +13,6 @@ describe 'Usuário acessa a tela inicial' do
     end
 
     it 'e visualiza os galpões cadastrados' do 
-
         #Arrange
         Warehouse.create(name: 'Rio', cod: 'SDU', city: 'Rio de Janeiro', area: 50_000)
         Warehouse.create(name: 'Maceio', cod: 'MCZ', city: 'Maceió', area: 60_000)
@@ -22,6 +21,7 @@ describe 'Usuário acessa a tela inicial' do
         visit(root_path)
 
         #Assert
+        expect(page).not_to have_content("Não há galpões cadastrados")
         expect(page).to have_content("Nome: Rio")
         expect(page).to have_content("Código: SDU")
         expect(page).to have_content("Cidade: Rio de Janeiro")
@@ -31,6 +31,18 @@ describe 'Usuário acessa a tela inicial' do
         expect(page).to have_content("Código: MCZ")
         expect(page).to have_content("Cidade: Maceió")
         expect(page).to have_content("Área: 60000 m²")
+    end
+
+    it 'e não existem galpões cadastrados' do 
+
+        #Arrange
+
+        #Act
+        visit(root_path)
+        
+        #Assert
+        expect(page).to have_content("Não há galpões cadastrados")
+
     end
 
 
