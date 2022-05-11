aee2401da17eea04fbc94b20f2e71ae1098155ba
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-05-11 13:57:55 -0300

    Adição de modelo de produto

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,3 +1,7 @@
 class ProductModel < ApplicationRecord
   belongs_to :supplier
+  validates :name, :sku, :weight, :width, :height, :depth, presence: true
+  validates :sku, length: {in: 20..20}
+  validates :sku, uniqueness: true
+  validates :weight, :width, :height, :depth, comparison: {greater_than: 0}
 end
--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -1,5 +1,114 @@
 require 'rails_helper'
 
 RSpec.describe ProductModel, type: :model do
+
+    describe '#valid?' do
+
+        it 'falso quando nome está vazio' do
+
+            @supplier = Supplier.create!(corporation_name: 'Samsung Brasil LTDA', brand_name: 'Samsung', registration_number: '12.345.678/1000-10',
+                address: 'Rua Progresso, 2548', city: 'Blumenau', state: 'Santa Catarina', email: 'inoxbrasilcontato@inox.com.br')
+    
+            @product = ProductModel.create(name: '', weight: 2000, width: 70, height: 45, depth: 10, sku:'TV32I-SAMSU-XPTZXO90', supplier: @supplier)
+            
+            result = @product.valid?
+
+            expect(result).to eq(false)
+        
+        end
+
+        it 'falso quando peso está vazio' do
+
+            @supplier = Supplier.create!(corporation_name: 'Samsung Brasil LTDA', brand_name: 'Samsung', registration_number: '12.345.678/1000-10',
+                address: 'Rua Progresso, 2548', city: 'Blumenau', state: 'Santa Catarina', email: 'inoxbrasilcontato@inox.com.br')
+    
+            @product = ProductModel.create(name: 'TV-32 polegadas', weight: '', width: 70, height: 45, depth: 10, sku:'TV32I-SAMSU-XPTZXO90', supplier: @supplier)
+            
+            result = @product.valid?
+
+            expect(result).to eq(false)
+        
+        end
+
+        it 'falso quando largura está vazio' do
+
+            @supplier = Supplier.create!(corporation_name: 'Samsung Brasil LTDA', brand_name: 'Samsung', registration_number: '12.345.678/1000-10',
+                address: 'Rua Progresso, 2548', city: 'Blumenau', state: 'Santa Catarina', email: 'inoxbrasilcontato@inox.com.br')
+    
+            @product = ProductModel.create(name: 'TV-32 polegadas', weight: 2000, width: '', height: 45, depth: 10, sku:'TV32I-SAMSU-XPTZXO90', supplier: @supplier)
+            
+            result = @product.valid?
+
+            expect(result).to eq(false)
+        
+        end
+
+
+        it 'falso quando codigo é menor que 20' do
+
+            @supplier = Supplier.create!(corporation_name: 'Samsung Brasil LTDA', brand_name: 'Samsung', registration_number: '12.345.678/1000-10',
+                address: 'Rua Progresso, 2548', city: 'Blumenau', state: 'Santa Catarina', email: 'inoxbrasilcontato@inox.com.br')
+    
+            @product = ProductModel.create(name: 'TV-Samsung 32in', weight: 2000, width: 70, height: 45, depth: 10, sku:'TV32I-SAMU-XPTZXO90', supplier: @supplier)
+            
+            result = @product.valid?
+
+            expect(result).to eq(false)
+        
+        end
+
+        it 'falso quando peso é menor ou igual a 0' do
+
+            @supplier = Supplier.create!(corporation_name: 'Samsung Brasil LTDA', brand_name: 'Samsung', registration_number: '12.345.678/1000-10',
+                address: 'Rua Progresso, 2548', city: 'Blumenau', state: 'Santa Catarina', email: 'inoxbrasilcontato@inox.com.br')
+    
+            @product = ProductModel.create(name: 'TV-Samsung 32in', weight: -1, width: 70, height: 45, depth: 10, sku:'TV32I-SAMU-XPTZZERXO', supplier: @supplier)
+            
+            result = @product.valid?
+
+            expect(result).to eq(false)
+        
+        end
+
+        it 'falso quando largura é menor ou igual a 0' do
+
+            @supplier = Supplier.create!(corporation_name: 'Samsung Brasil LTDA', brand_name: 'Samsung', registration_number: '12.345.678/1000-10',
+                address: 'Rua Progresso, 2548', city: 'Blumenau', state: 'Santa Catarina', email: 'inoxbrasilcontato@inox.com.br')
+    
+            @product = ProductModel.create(name: 'TV-Samsung 32in', weight: 2000, width: 0, height: 45, depth: 10, sku:'TV32I-SAMU-XPTZZERXO', supplier: @supplier)
+            
+            result = @product.valid?
+
+            expect(result).to eq(false)
+        
+        end
+
+
+        it 'falso quando altura é menor ou igual a 0' do
+
+            @supplier = Supplier.create!(corporation_name: 'Samsung Brasil LTDA', brand_name: 'Samsung', registration_number: '12.345.678/1000-10',
+                address: 'Rua Progresso, 2548', city: 'Blumenau', state: 'Santa Catarina', email: 'inoxbrasilcontato@inox.com.br')
+    
+            @product = ProductModel.create(name: 'TV-Samsung 32in', weight: 2000, width: 70, height: -2, depth: 10, sku:'TV32I-SAMU-XPTZZERXO', supplier: @supplier)
+            
+            result = @product.valid?
+
+            expect(result).to eq(false)
+        
+        end
+
+        it 'falso quando profundidade é menor ou igual a 0' do
+
+            @supplier = Supplier.create!(corporation_name: 'Samsung Brasil LTDA', brand_name: 'Samsung', registration_number: '12.345.678/1000-10',
+                address: 'Rua Progresso, 2548', city: 'Blumenau', state: 'Santa Catarina', email: 'inoxbrasilcontato@inox.com.br')
+    
+            @product = ProductModel.create(name: 'TV-Samsung 32in', weight: 2000, width: 70, height: 20, depth: 0, sku:'TV32I-SAMU-XPTZZERXO', supplier: @supplier)
+            
+            result = @product.valid?
+
+            expect(result).to eq(false)
+        
+        end
+    end
   
 end
